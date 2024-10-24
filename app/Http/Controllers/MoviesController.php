7ed38f1b6357e73340732dca4a3ab06369e8cd47
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-24 19:33:31 -0400

    db in MoviesController

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MoviesController extends Controller
 {
     //metodos para gestir a route
     public function index(){
+
+        $movies = Movie::all();
+        dd($movies);
         return view("movies.index");
     }
-
 
 }
