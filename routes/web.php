720e9501a74ac97d99b0229dc4d51e1d7c054fa0
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-24 18:50:08 -0400

    web.php "MoviesController"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+// Crio um metodo para falar com o controller
+Route::get("/movies", [MoviesController::class, "index"]);
